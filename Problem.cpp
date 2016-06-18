b505528d758de36ad0dddd373605e9b7a773f98a
Roshan Gautam <roshan.gautam@hotmail.com>
2016-06-18 07:59:20 -0500

    Fix the set subtraction logic

--- Problem.cpp
+++ Problem.cpp
@@ -17,74 +17,73 @@ Problem::Problem() {
     cout << "Zebra Water Problem Initialized" << endl;
 
     //The Englishman lives in the red house.
-    colorsNationality[RED].clear();
     colorsNationality[RED] = ENGLISH;
-    colorsNationality[GREEN] = colorsNationality[GREEN] - colorsNationality[RED];
-    colorsNationality[BLUE] = colorsNationality[BLUE] - colorsNationality[RED];
-    colorsNationality[YELLOW] = colorsNationality[YELLOW] -  colorsNationality[RED];
-    colorsNationality[WHITE] = colorsNationality[WHITE] - colorsNationality[RED];
+    colorsNationality[GREEN] = colorsNationality[RED] - colorsNationality[GREEN];
+    colorsNationality[BLUE] = colorsNationality[RED] - colorsNationality[BLUE];
+    colorsNationality[YELLOW] = colorsNationality[RED] - colorsNationality[YELLOW];
+    colorsNationality[WHITE] = colorsNationality[RED] - colorsNationality[WHITE];
 
     //The Spaniard owns the dog.
     petsNationality[DOG] = SPANISH;
-    petsNationality[HORSE] = petsNationality[HORSE] - petsNationality[DOG];
-    petsNationality[SNAILS] = petsNationality[SNAILS] - petsNationality[DOG];
-    petsNationality[FOX] = petsNationality[FOX] - petsNationality[DOG];
-    petsNationality[ZEBRA] = petsNationality[ZEBRA] - petsNationality[DOG];
+    petsNationality[HORSE] = petsNationality[DOG] - petsNationality[HORSE];
+    petsNationality[SNAILS] = petsNationality[DOG] - petsNationality[SNAILS];
+    petsNationality[FOX] = petsNationality[DOG] - petsNationality[FOX];
+    petsNationality[ZEBRA] = petsNationality[DOG] - petsNationality[ZEBRA];
 
     //Coffee is drunk in the green house.
     drinksColors[COFFEE] = GREEN;
-    drinksColors[MILK] = drinksColors[MILK] - drinksColors[COFFEE];
-    drinksColors[WATER] = drinksColors[WATER] - drinksColors[COFFEE];
-    drinksColors[OJ] = drinksColors[OJ] - drinksColors[COFFEE];
-    drinksColors[TEA] = drinksColors[TEA] - drinksColors[COFFEE];
+    drinksColors[MILK] = drinksColors[COFFEE] - drinksColors[MILK];
+    drinksColors[WATER] = drinksColors[COFFEE] - drinksColors[WATER];
+    drinksColors[OJ] = drinksColors[COFFEE] - drinksColors[OJ];
+    drinksColors[TEA] = drinksColors[COFFEE] - drinksColors[TEA];
 
     //The Ukrainian drinks tea.
     drinksNationality[UKRANIAN] = TEA;
-    drinksNationality[NORWEGIAN] = drinksNationality[NORWEGIAN] - drinksNationality[UKRANIAN];
-    drinksNationality[ENGLISH] = drinksNationality[ENGLISH] - drinksNationality[UKRANIAN];
-    drinksNationality[SPANISH] = drinksNationality[SPANISH] - drinksNationality[UKRANIAN];
-    drinksNationality[JAPANESE] = drinksNationality[JAPANESE] - drinksNationality[UKRANIAN];
+    drinksNationality[NORWEGIAN] = drinksNationality[UKRANIAN] - drinksNationality[NORWEGIAN];
+    drinksNationality[ENGLISH] = drinksNationality[UKRANIAN] - drinksNationality[ENGLISH];
+    drinksNationality[SPANISH] = drinksNationality[UKRANIAN] - drinksNationality[SPANISH];
+    drinksNationality[JAPANESE] = drinksNationality[UKRANIAN] - drinksNationality[JAPANESE];
 
     //The Old Gold smoker owns snails.
     petsCigarettes[SNAILS] = OLD_GOLDS;
-    petsCigarettes[DOG] = petsCigarettes[DOG] - petsCigarettes[SNAILS];
-    petsCigarettes[FOX] = petsCigarettes[FOX] - petsCigarettes[SNAILS];
-    petsCigarettes[HORSE] = petsCigarettes[HORSE] - petsCigarettes[SNAILS];
-    petsCigarettes[ZEBRA] = petsCigarettes[ZEBRA] - petsCigarettes[SNAILS];
+    petsCigarettes[DOG] = petsCigarettes[SNAILS] - petsCigarettes[DOG];
+    petsCigarettes[FOX] = petsCigarettes[SNAILS] - petsCigarettes[FOX];
+    petsCigarettes[HORSE] = petsCigarettes[SNAILS] - petsCigarettes[HORSE];
+    petsCigarettes[ZEBRA] = petsCigarettes[SNAILS] - petsCigarettes[ZEBRA];
 
     //Kools are smoked in the yellow house.
     cigarettesColors[KOOLS] = YELLOW;
-    cigarettesColors[LUCKY_STRIKE] = cigarettesColors[LUCKY_STRIKE] - cigarettesColors[KOOLS];
-    cigarettesColors[OLD_GOLDS] = cigarettesColors[OLD_GOLDS] - cigarettesColors[KOOLS];
-    cigarettesColors[CHESTERFIELDS] = cigarettesColors[CHESTERFIELDS] - cigarettesColors[KOOLS];
-    cigarettesColors[PARLIAMENTS] = cigarettesColors[PARLIAMENTS] - cigarettesColors[KOOLS];
+    cigarettesColors[LUCKY_STRIKE] = cigarettesColors[KOOLS] - cigarettesColors[LUCKY_STRIKE];
+    cigarettesColors[OLD_GOLDS] = cigarettesColors[KOOLS] - cigarettesColors[OLD_GOLDS];
+    cigarettesColors[CHESTERFIELDS] = cigarettesColors[KOOLS] - cigarettesColors[CHESTERFIELDS];
+    cigarettesColors[PARLIAMENTS] = cigarettesColors[KOOLS] - cigarettesColors[PARLIAMENTS];
 
 
     //Milk is drunk in the middle house.
     drinksPosition[MILK] = THIRD;
     drinksPosition[OJ] = drinksPosition[MILK] - drinksPosition[MILK];
-    drinksPosition[WATER] = drinksPosition[WATER] - drinksPosition[MILK];
-    drinksPosition[TEA] = drinksPosition[TEA] - drinksPosition[MILK];
-    drinksPosition[COFFEE] = drinksPosition[COFFEE] - drinksPosition[MILK];
+    drinksPosition[WATER] = drinksPosition[MILK] - drinksPosition[WATER];
+    drinksPosition[TEA] = drinksPosition[MILK] - drinksPosition[TEA];
+    drinksPosition[COFFEE] = drinksPosition[MILK] - drinksPosition[COFFEE];
 
     //The Norwegian lives in the first house.
     positionNationality[FIRST] = NORWEGIAN;
-    positionNationality[SECOND] = positionNationality[SECOND] - positionNationality[FIRST];
-    positionNationality[THIRD] = positionNationality[THIRD] - positionNationality[FIRST];
-    positionNationality[FOURTH] = positionNationality[FOURTH] - positionNationality[FIRST];
-    positionNationality[FIFTH] = positionNationality[FIFTH] - positionNationality[FIRST];
+    positionNationality[SECOND] = positionNationality[FIRST] - positionNationality[SECOND];
+    positionNationality[THIRD] = positionNationality[FIRST] - positionNationality[THIRD];
+    positionNationality[FOURTH] = positionNationality[FIRST] - positionNationality[FOURTH];
+    positionNationality[FIFTH] = positionNationality[FIRST] - positionNationality[FIFTH];
 
     //The Lucky Strike smoker drinks orange juice.
     cigarettesDrinks[LUCKY_STRIKE] = OJ;
-    cigarettesDrinks[CHESTERFIELDS] = cigarettesDrinks[CHESTERFIELDS] - cigarettesDrinks[LUCKY_STRIKE];
-    cigarettesDrinks[KOOLS] = cigarettesDrinks[KOOLS] - cigarettesDrinks[LUCKY_STRIKE];
-    cigarettesDrinks[PARLIAMENTS] = cigarettesDrinks[PARLIAMENTS] - cigarettesDrinks[LUCKY_STRIKE];
-    cigarettesDrinks[OLD_GOLDS] = cigarettesDrinks[OLD_GOLDS] - cigarettesDrinks[LUCKY_STRIKE];
+    cigarettesDrinks[CHESTERFIELDS] = cigarettesDrinks[LUCKY_STRIKE] - cigarettesDrinks[CHESTERFIELDS];
+    cigarettesDrinks[KOOLS] = cigarettesDrinks[LUCKY_STRIKE] - cigarettesDrinks[KOOLS];
+    cigarettesDrinks[PARLIAMENTS] = cigarettesDrinks[LUCKY_STRIKE] - cigarettesDrinks[PARLIAMENTS];
+    cigarettesDrinks[OLD_GOLDS] = cigarettesDrinks[LUCKY_STRIKE] - cigarettesDrinks[OLD_GOLDS];
 
     // The Japanese smokes Parliaments.
     nationalityCigarettes[JAPANESE] = PARLIAMENTS;
-    nationalityCigarettes[NORWEGIAN] = nationalityCigarettes[NORWEGIAN] - nationalityCigarettes[JAPANESE];
-    nationalityCigarettes[ENGLISH] = nationalityCigarettes[ENGLISH] - nationalityCigarettes[JAPANESE];
-    nationalityCigarettes[SPANISH] = nationalityCigarettes[SPANISH] - nationalityCigarettes[JAPANESE];
-    nationalityCigarettes[UKRANIAN] = nationalityCigarettes[UKRANIAN] - nationalityCigarettes[JAPANESE];
+    nationalityCigarettes[NORWEGIAN] = nationalityCigarettes[JAPANESE] - nationalityCigarettes[NORWEGIAN];
+    nationalityCigarettes[ENGLISH] = nationalityCigarettes[JAPANESE] - nationalityCigarettes[ENGLISH];
+    nationalityCigarettes[SPANISH] = nationalityCigarettes[JAPANESE]- nationalityCigarettes[SPANISH];
+    nationalityCigarettes[UKRANIAN] = nationalityCigarettes[JAPANESE] - nationalityCigarettes[UKRANIAN];
 }
